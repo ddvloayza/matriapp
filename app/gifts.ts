cb807930f93c10d9d79649f79ef5cd429b89e0be
Intelica-JulioMartinez <julio.martinez@intelica.com>
2026-07-28 18:16:04 -0500

    Marcar 6 regalos como ya obsequiados

--- app/gifts.ts
+++ app/gifts.ts
@@ -9,16 +9,16 @@ export type Gift = {
 
 export const gifts: Gift[] = [
   { name: "Cama Queen", detail: "Cabecera + Tarima color plomo", taken: false },
-  { name: "Colchón para cama Queen", detail: "Paraíso", taken: false },
+  { name: "Colchón para cama Queen", detail: "Paraíso", taken: true },
   { name: "Sofá para la sala forma L", detail: "Consultar medidas", taken: false },
   { name: "Centro de Entretenimiento", detail: "Consultar medidas", taken: false },
-  { name: "Mesa de comedor + sillas", detail: "6 sillas", taken: false },
+  { name: "Mesa de comedor + sillas", detail: "6 sillas", taken: true },
   {
     name: "Refrigeradora marca LG o Samsung",
     detail: "Consultar medidas",
     taken: false
   },
-  { name: 'Televisor para Sala 65" o 75" marca LG', taken: false },
+  { name: 'Televisor para Sala 65" o 75" marca LG', taken: true },
   { name: "Soundbar marca LG", taken: false },
   { name: "Terma a Gas marca Sole", taken: false },
   { name: "Olla a presión marca Record o Oster", taken: false },
@@ -29,8 +29,8 @@ export const gifts: Gift[] = [
   },
   { name: "Licuadora marca Oster", detail: "Vaso de vidrio", taken: false },
   { name: "Cafetera + Waflera + Hervidor eléctrico", taken: false },
-  { name: "Freidora de aire marca Oster o Imaco", taken: false },
-  { name: "Juego de ollas Tramontina", taken: false },
+  { name: "Freidora de aire marca Oster o Imaco", taken: true },
+  { name: "Juego de ollas Tramontina", taken: true },
   { name: "Juego de Sartenes de Teflón", taken: false },
   { name: "Ventilador + Aspiradora", taken: false },
   { name: "Arrocera 3 Kg + Batidora", taken: false },
@@ -39,7 +39,7 @@ export const gifts: Gift[] = [
   { name: "Juego de Cuchillos de Acero Inoxidable marca Tramontina", taken: false },
   { name: "Juego de Cubiertos marca Tramontina", taken: false },
   { name: "Toallas", detail: "Set completo · Colores claros", taken: false },
-  { name: "Juego de Copas para vino y champán", taken: false },
+  { name: "Juego de Copas para vino y champán", taken: true },
   {
     name: "Organizadores de Bambú",
     detail: "Para clóset, baño y dormitorio",
